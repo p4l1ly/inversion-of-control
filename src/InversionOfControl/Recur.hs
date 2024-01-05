2b0f1848d2a70e02f1647b13cc2fff1cb0264181
Pavol Vargovčík <pavol.vargovcik@gmail.com>
2024-01-05 13:07:48 +0100

    simple HyloRecur

--- src/InversionOfControl/Recur.hs
+++ src/InversionOfControl/Recur.hs
@@ -6,9 +6,11 @@
 
 module InversionOfControl.Recur where
 
+import Control.Monad
 import Control.Monad.Trans
 import InversionOfControl.Lift
 import InversionOfControl.LiftN
+-- import Data.Functor.Compose
 
 data Explicit (k :: K) (n' :: Pean) (r :: *) (fr :: *)
 type family Ek e :: K where
@@ -28,4 +30,34 @@ class (Monad m, MonadTrans (Et e), Monad (Et e m)) => Recur e b m where
     (Monad m', LiftN (En' e) (Et e m) m') =>
     ((Er e -> m' b) -> Efr e -> m' b) ->
     ((Er e -> m' b) -> Et e m a) ->
+    m a
+
+newtype BeforeAfter m a = BeforeAfter (m (m a))
+
+instance Functor m => Functor (BeforeAfter m) where
+  fmap f (BeforeAfter bef) = BeforeAfter (fmap (fmap f) bef)
+
+instance Applicative m => Applicative (BeforeAfter m) where
+  pure a = BeforeAfter (pure (pure a))
+  BeforeAfter bef1 <*> BeforeAfter bef2 = BeforeAfter $ (<*>) <$> bef1 <*> bef2
+
+runBeforeAfter :: Monad m => BeforeAfter m a -> m a
+runBeforeAfter (BeforeAfter bef) = join bef
+
+-- class (Monad m, Applicative (Et e m), Monoid p) => HyloRecur e b p m where
+--   -- TODO support composed applicative functors where (Et e m) is (En' e) levels deep
+--   runHyloRecur ::
+--     ((Er e -> m ()) -> Efr e -> m ()) ->
+--     ((Er e -> p -> Et e m b) -> Efr e -> p -> Et e m b) ->
+--     ((Er e -> m ()) -> m ()) ->
+--     ((Er e -> p -> Et e m b) -> Et e m a) ->
+--     m a
+
+-- TODO this is very specific around BeforeAfter
+class (Monad m, Monoid p) => HyloRecur e b p m where
+  runHyloRecur ::
+    ((Er e -> m ()) -> Efr e -> m ()) ->
+    ((Er e -> p -> BeforeAfter m b) -> Efr e -> p -> BeforeAfter m b) ->
+    ((Er e -> m ()) -> m ()) ->
+    ((Er e -> p -> BeforeAfter m b) -> BeforeAfter m a) ->
     m a
